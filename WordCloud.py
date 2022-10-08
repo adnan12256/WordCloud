f354ecceafe6ee4be1a93c36d06e5c564e450491
adnan12256 <94077298+adnan12256@users.noreply.github.com>
2022-10-08 17:07:20 -0400

    Update WordCloud.py

--- WordCloud.py
+++ WordCloud.py
@@ -25,14 +25,11 @@ def calculate_frequencies(file_contents):
     # Here is a list of punctuations and uninteresting words you can use to process your text
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my",
-                           "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its",
-                           "they", "them",
-                           "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be",
-                           "been", "being",
-                           "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when",
-                           "where", "how",
-                           "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very",
-                           "can", "will", "just", "in", "on", "for"]
+                           "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers",
+                           "its","they", "them","their", "what", "which", "who", "whom", "this", "that", "am", "are",
+                           "was", "were", "be","been", "being", "have", "has", "had", "do", "does", "did", "but", "at",
+                           "by", "with", "from", "here", "when","where", "how", "all", "any", "both", "each", "few", "more",
+                           "some", "such", "no", "nor", "too", "very","can", "will", "just", "in", "on", "for"]
 
     new_content = ""
     for index, i in enumerate(file_contents):
